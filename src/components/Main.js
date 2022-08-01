309c0fc9dd4877b06aeeb3a4541bdaab66291d79
Nikita Mamaev <palmally@yandex.ru>
2022-08-01 20:14:27 +0300

    loading cards v1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,35 +1,61 @@
-import spinnerImage from '../images/loader.svg';
+import { useState, useEffect } from 'react';
+import api from '../utills/api';
 
 function Main(props) {
-  
+  const [userName, setUserName] = useState(undefined);
+  const [userDescription, setUserDescription] = useState(undefined);
+  const [userAvatar, setUserAvatar] = useState(undefined);
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    api.getUserInfo()
+      .then(({ name, about, avatar }) => {
+        setUserName(name);
+        setUserDescription(about);
+        setUserAvatar(avatar);
+      })
+      .catch((err) => console.log(`ошибка при загрузке профиля: ${err}`))
+  }, []);
+
+  useEffect(() => {
+    api.getInitialCards()
+      .then((cardsData) => {
+        setCards(
+          cardsData.map(({ name, link, likes, _id }) => {
+            return (
+              <li className="photo-card" key={_id}>
+                <div className="photo-card__caption">
+                  <h2 className="photo-card__title">{name}</h2>
+                  <div className="photo-card__likes-wrapper">
+                    <button type="button" className="photo-card__like-button" aria-label="like image"></button>
+                    <span className="photo-card__likes-counter">{likes.length}</span>
+                  </div>  
+                </div>
+                <img src={link} alt={name} className="photo-card__image" />
+                <button type="button" className="photo-card__delete-button" aria-label="delete card"></button>
+              </li>)
+          })
+        );
+      })
+      .catch((err) => console.log(`ошибка при загрузке одной или нескольких карточек: ${err}`));
+  }, [])
+
   return (
     <main className="content">
       <section className="profile" type="button">
         <button className="profile__avatar-wrapper" onClick={props.onEditAvatar}>
-          <img src={spinnerImage} alt="Аватар профиля" className="profile__avatar" />
+          <img src={userAvatar} alt="Аватар профиля" className="profile__avatar" />
         </button>  
         <div className="profile__info">
-          <h1 className="profile__name">Никита Мамаев</h1>
+          <h1 className="profile__name">{userName}</h1>
           <button type="button" className="profile__edit-button" aria-label="edit profile" onClick={props.onEditProfile}></button>
-          <p className="profile__about">Путешественник</p>
+          <p className="profile__about">{userDescription}</p>
         </div>
         <button type="button" className="profile__add-button" aria-label="add card" onClick={props.onAddPlace}></button>
       </section>
       <section className="elements">
         <ul className="elements__list">
-          <template id="photo-card-template">
-            <li className="photo-card">
-              <div className="photo-card__caption">
-                <h2 className="photo-card__title"></h2>
-                <div className="photo-card__likes-wrapper">
-                  <button type="button" className="photo-card__like-button" aria-label="like image"></button>
-                  <span className="photo-card__likes-counter">0</span>
-                </div>  
-              </div>
-              <img src="#" alt="#" className="photo-card__image" />
-              <button type="button" className="photo-card__delete-button" aria-label="delete card"></button>
-            </li>
-          </template>
+          {cards}
         </ul>
       </section>
     </main>
---
+++ src/utills/api.js
@@ -0,0 +1,94 @@
+class Api {
+  constructor({baseUrl, headers}) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _handleResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`код ${res.status}`); //обработка ответа от сервера повторяется, решил выделить в отдельный метод
+  }
+
+  _getData(path) {
+    return fetch(`${this._baseUrl}${path}`, {
+      headers: {
+        authorization: this._headers.authorization
+      }
+    })
+      .then(res => this._handleResponse(res));
+  }
+
+  getUserInfo() {
+    return this._getData('/users/me'); 
+  }
+
+  getInitialCards() {
+    return this._getData('/cards');
+  }
+
+  _changeData(data, path) {
+    return fetch(`${this._baseUrl}${path}`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify(data)
+    })
+      .then(res => this._handleResponse(res));
+  }
+
+  changeUserInfo(data) {
+    return this._changeData(data, '/users/me');
+  }
+  
+  changeAvatar(data) {
+    return this._changeData(data, '/users/me/avatar');
+  }
+  
+  addCard(data) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(data)
+    })
+    .then(res => this._handleResponse(res));
+  }
+
+  _handleLike(method, id) {
+    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+      method: method,
+      headers: {
+        authorization: this._headers.authorization
+      }
+    })
+      .then(res => this._handleResponse(res))
+  }
+
+  setLike(id) {
+    return this._handleLike('PUT', id)
+  }
+
+  removeLike(id) {
+    return this._handleLike('DELETE', id)
+  }
+
+  deleteCard(id) {
+    return fetch(`${this._baseUrl}/cards/${id}`, {
+      method: 'DELETE',
+      headers: {
+        authorization: this._headers.authorization
+      }
+    })
+    .then(res => this._handleResponse(res))
+  }  
+}
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-47',
+  headers: {
+    authorization: 'c057a3e7-5ee0-421c-b032-822b62f6abd9',
+    'Content-Type': 'application/json'
+  }
+});
+
+export default api;
